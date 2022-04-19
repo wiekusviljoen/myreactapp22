70ebb0aa71b1951d27f6509daefff5cbe5e49a21
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-19 20:16:41 +0200

    commit on master

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AboutView from "./components/AboutView";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Switch>
+      <Routes>
         <Route path="/" exact>
           <Home />
         </Route>
         <Route path="/about" component={AboutView} />
-      </Switch>
+      </Routes>
     </div>
   );
 }
