f732401ae63fc87d9894dbdd8eda788ff51ba1f0
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-19 19:20:01 +0200

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-import AboutView from "./components/AboutView";
+import AboutView from "./pages/AboutView";
 
 function App() {
   return (
