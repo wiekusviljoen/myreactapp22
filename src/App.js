b9a8a9f3f29964fec977efb3218602250fb0d72a
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-19 12:53:03 +0200

    commit on master

--- src/App.js
+++ src/App.js
@@ -9,12 +9,26 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <div class="form-floating">
+        <select
+          class="form-select"
+          id="floatingSelect"
+          aria-label="Floating label select example"
+        >
+          <option selected>Open this select menu</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+        <label for="floatingSelect">Works with selects</label>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
