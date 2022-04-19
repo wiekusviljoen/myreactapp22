75a00d476b4e2227999d9b5d0037db368261dd4d
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-19 18:26:57 +0200

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
+        <a className="navbar-brand" href="gotosomewhere ">
+          Movie Browser
         </a>
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
+          data-bs-target="gotosomewhere navbarSupportedContent"
           aria-controls="navbarSupportedContent"
           aria-expanded="false"
           aria-label="Toggle navigation"
@@ -22,12 +21,16 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="gotosomewhere "
+              >
                 Home
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="gotosomewhere ">
                 Link
               </a>
             </li>
