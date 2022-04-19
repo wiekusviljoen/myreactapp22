bfcbd92b849d08b291d8f3527d3e42d7640730e4
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-19 19:15:14 +0200

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import AboutView from "./components/AboutView";
 
 function App() {
   return (
     <div classNameName="App">
       <Navbar />
       <Home />
+      <AboutView />
     </div>
   );
 }
---
+++ src/components/AboutView.js
@@ -0,0 +1,9 @@
+const AboutView = () => {
+  return (
+    <>
+      <h2>About us</h2>
+    </>
+  );
+};
+
+export default AboutView;
