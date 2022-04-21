6e34e96df11a0ef64daa4c7f8e1e127fc94b830a
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-21 18:36:26 +0200

    commit on master

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,10 @@
-import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">
+        <a href className="navbar-brand" to="/">
           Movie Browser
-        </Link>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,23 +18,23 @@ const Navbar = () => {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
+            <a href="/">
+              <button type="button" class="button">
                 Home
-              </Link>
-            </li>
+              </button>
+            </a>
 
-            <li className="nav-item">
-              <Link className="nav-link active" to="/about ">
+            <a href="/about">
+              <button type="button" class="button">
                 About
-              </Link>
-            </li>
+              </button>
+            </a>
 
-            <li className="nav-item">
-              <Link className="nav-link active" to="/">
+            <a href="/">
+              <button type="button" class="button">
                 Coming Soon
-              </Link>
-            </li>
+              </button>
+            </a>
 
             <li className="nav-item"></li>
           </ul>
