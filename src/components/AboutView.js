196d037539594838b0e6c0d6a215b679c2ac6e0a
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-22 11:32:03 +0200

    commit on master

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -2,8 +2,8 @@ import Hero from "./Hero";
 const AboutView = () => {
   return (
     <>
-      <Hero text="Welcome to React 201" />
-      <h1>About us </h1>
+      <Hero text="About us" />
+
       <div className="container">
         <div className="row">
           <div className="col-lg-8 offset-lg-2 my-5">
