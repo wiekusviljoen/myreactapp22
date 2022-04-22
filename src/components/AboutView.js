96fdbe77e4cd91f34fa3201293e0c67d1292f78d
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-22 11:25:53 +0200

    commit on master

--- src/components/AboutView.js
+++ src/components/AboutView.js
@@ -1,10 +1,20 @@
 import Hero from "./Hero";
 const AboutView = () => {
   return (
-    <h2>
-      <Hero />
-      About us
-    </h2>
+    <>
+      <Hero text="Welcome to React 201" />
+      <h1>About us </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <p className="Lead">
+              About page dlafj lafkj lasfkjsdkjf a ldsf lskdf lajlf jlasjfdl
+              ajslf
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
