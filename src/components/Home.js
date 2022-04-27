235cde19c65ec366a82681822a3234f4374a11e9
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-27 21:21:21 +0200

    commit on master

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
     <>
       <Hero text="Welcome To FrogIns Transport" />
       <h1>Hello world from react 2022</h1>
-      <div class="paragraphHome" className="container">
+      <div class="paragraphHome">
         Lorem slkfjalsjflakjflkslf lskfj laf lafjd lsjdf lsf kjsf
         lkdlajfsldkflks lalsdkf ks ldafj flkadsfl jslf slkdfj alkjdf als
       </div>
