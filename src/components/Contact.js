dc01b04a8ae851f41ee91d20b06e7a189d9c2ee7
wiekusviljoen <wiekusviljoen@gmail.com>
2022-04-27 21:28:20 +0200

    commit on master

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,15 +3,9 @@ const Contact = () => {
   return (
     <>
       <Hero text="Contact" />
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8 offset-lg-2 my-5">
-            <p className="Lead">
-              About page dlafj lafkj lasfkjsdkjf a ldsf lskdf lajlf jlasjfdl
-              ajslf
-            </p>
-          </div>
-        </div>
+
+      <div class="paragraphContact">
+        dlafj lafkj lasfkjsdkjf a ldsf lskdf lajlf jlasjfdl ajslf
       </div>
     </>
   );
