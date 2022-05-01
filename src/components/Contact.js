581b7a813186ffe6212e978a78964dd44729965e
wiekusviljoen <wiekusviljoen@gmail.com>
2022-05-01 20:03:13 +0200

    commit on master

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,45 @@
 import Hero from "./Hero";
-const Contact = () => {
-  return (
-    <>
-      <Hero text="Contact" />
 
-      <div class="paragraphContact">
-        dlafj lafkj lasfkjsdkjf a ldsf lskdf lajlf jlasjfdl ajslf
+import React, { useState } from "react";
+<Hero text="Contact" />;
+const Contact = () => {
+  const [status, setStatus] = useState("Submit");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("Sending...");
+    const { name, email, message } = e.target.elements;
+    let details = {
+      name: name.value,
+      email: email.value,
+      message: message.value,
+    };
+    let response = await fetch("http://localhost:5000/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json;charset=utf-8",
+      },
+      body: JSON.stringify(details),
+    });
+    setStatus("Submit");
+    let result = await response.json();
+    alert(result.status);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" required />
       </div>
-    </>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" required />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea id="message" required />
+      </div>
+      <button type="submit">{status}</button>
+    </form>
   );
 };
 
