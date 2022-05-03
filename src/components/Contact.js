ee83679ba7bbae9ea7b750aceaf1969845959889
wiekusviljoen <wiekusviljoen@gmail.com>
2022-05-03 16:39:55 +0200

    commit on master

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,53 +1,47 @@
-import Hero from "./Hero";
-import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  <>
-    <Hero text="Contact us" />
-
-    <div class="paragraphContact">
-      dlafj lafkj lasfkjsdkjf a ldsf lskdf lajlf jlasjfdl ajslf
-    </div>
-  </>;
-
-  const [status, setStatus] = useState("Submit");
-  const handleSubmit = async (e) => {
+  function sendEmail(e) {
     e.preventDefault();
-    setStatus("Sending...");
-    const { name, email, message } = e.target.elements;
-    let details = {
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    };
-    let response = await fetch("http://localhost:5000/contact", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-      },
-      body: JSON.stringify(details),
-    });
-    setStatus("Submit");
-    let result = await response.json();
-    alert(result.status);
-  };
+
+    emailjs
+      .sendForm("service_z4x1kcc", "template_z1sy47v", e.target)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" required />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input type="email" id="email" required />
-      </div>
-      <div>
-        <label htmlFor="message">Message:</label>
-        <textarea id="message" required />
-      </div>
-      <button type="submit">{status}</button>
-    </form>
+    <div>
+      className="container border" style=
+      {{
+        marginTop: "50px",
+        width: "50px",
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+      <h1 style={{ marginTop: "25px" }}>Contact Form</h1>
+      <form
+        className="row"
+        style={{ margin: "25px 85px 75px 100px" }}
+        onSubmit={sendEmail}
+      >
+        <label>name</label>
+        <input type="text" name="name" className="from-control" />
+
+        <label>Email</label>
+        <input type="email" name="user_mail" className="form-control" />
+
+        <label>Message</label>
+        <textarea name="message" rows="4" className="form-control" />
+        <input
+          type="submit"
+          value="Send"
+          className="form-control btn btn-primary"
+          style={{ marginTop: "30px" }}
+        />
+      </form>
+    </div>
   );
 };
-
 export default Contact;
