a5a0db7142006079adc6fe22f5884f137e70c50d
wiekusviljoen <wiekusviljoen@gmail.com>
2022-05-07 22:27:38 +0200

    commit on master

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,6 +3,13 @@ const Services = () => {
   return (
     <>
       <Hero text="Services" />
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2 my-5">
+            <p className="Lead">Services Page ajslf</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
