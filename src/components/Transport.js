f6ac8420c396def01ec8d5983032aa8e096ab34f
wiekusviljoen <wiekusviljoen@gmail.com>
2022-05-07 22:30:28 +0200

    commit on master

--- src/components/Transport.js
+++ src/components/Transport.js
@@ -6,10 +6,7 @@ const Transport = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-8 offset-lg-2 my-5">
-            <p className="Lead">
-              About page dlafj lafkj lasfkjsdkjf a ldsf lskdf lajlf jlasjfdl
-              ajslf
-            </p>
+            <p className="Lead">Transport Page</p>
           </div>
         </div>
       </div>
