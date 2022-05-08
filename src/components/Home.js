0fd6aa305945407abb946e0996a24d41ef69ed83
wiekusviljoen <wiekusviljoen@gmail.com>
2022-05-08 17:47:41 +0200

    commit on master

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,11 @@ const Home = () => {
     <>
       <Hero text="Welcome" />
       <h1>Hello world from react 2022</h1>
-      <div class="image">
-        <img src={Frogins} />;
-      </div>
+      <section className="main-container">
+        <div className="image">
+          <img src={Frogins} />;
+        </div>
+      </section>
       <div class="paragraphHome">
         Lorem slkfjalsjflakjflkslf lskfj laf lafjd lsjdf lsf kjsf
         lkdlajfsldkflks lalsdkf ks ldafj flkadsfl jslf slkdfj alkjdf als
